ad8ddd5819f991a5749c906119787d6cb629c0ef
standragosexpleo <77485693+standragosexpleo@users.noreply.github.com>
2021-03-10 07:44:40 +0200

    Update face_recognition.py

--- polls/face_recognition.py
+++ polls/face_recognition.py
@@ -1,50 +1,50 @@
-import cv2
-import numpy as np
+# import cv2
+# import numpy as np
 
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout, Flatten
-from tensorflow.keras.layers import Conv2D
-from tensorflow.keras.layers import MaxPooling2D
+# from tensorflow.keras.models import Sequential
+# from tensorflow.keras.layers import Dense, Dropout, Flatten
+# from tensorflow.keras.layers import Conv2D
+# from tensorflow.keras.layers import MaxPooling2D
 
-import os
+# import os
 
 
-def get_emotion(photo):
-    model = Sequential()
-    model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(48, 48, 1)))
-    model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
-    model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
-    model.add(Flatten())
-    model.add(Dense(1024, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(7, activation='softmax'))
-    model.load_weights('polls/new_model.h5')
-    # prevents openCL usage and unnecessary logging messages
-    cv2.ocl.setUseOpenCL(False)
-    # dictionary which assigns each label an emotion (alphabetical order)
-    emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful", 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
-    # path catre imaginea pe care se face recunoasterea
-    frame = cv2.imread(photo)
-    facecasc = cv2.CascadeClassifier(os.path.join(cv2.data.haarcascades, 'haarcascade_frontalface_default.xml'))
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = facecasc.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y-50), (x+w, y+h+10), (255, 0, 0), 2)
-        roi_gray = gray[y:y + h, x:x + w]
-        cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
-        prediction = model.predict(cropped_img)
-        maxindex = int(np.argmax(prediction))
-        cv2.putText(frame, emotion_dict[maxindex], (frame.shape[0]//8, frame.shape[1]//8), cv2.FONT_HERSHEY_SIMPLEX, 1,
-                    (0, 255, 0), 2, cv2.LINE_AA)
-    # cv2.imshow('Recognition', frame)
-    cv2.waitKey()
-    return emotion_dict[maxindex]
+# def get_emotion(photo):
+#     model = Sequential()
+#     model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(48, 48, 1)))
+#     model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+#     model.add(MaxPooling2D(pool_size=(2, 2)))
+#     model.add(Dropout(0.25))
+#     model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+#     model.add(MaxPooling2D(pool_size=(2, 2)))
+#     model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+#     model.add(MaxPooling2D(pool_size=(2, 2)))
+#     model.add(Dropout(0.25))
+#     model.add(Flatten())
+#     model.add(Dense(1024, activation='relu'))
+#     model.add(Dropout(0.5))
+#     model.add(Dense(7, activation='softmax'))
+#     model.load_weights('polls/new_model.h5')
+#     # prevents openCL usage and unnecessary logging messages
+#     cv2.ocl.setUseOpenCL(False)
+#     # dictionary which assigns each label an emotion (alphabetical order)
+#     emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful", 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
+#     # path catre imaginea pe care se face recunoasterea
+#     frame = cv2.imread(photo)
+#     facecasc = cv2.CascadeClassifier(os.path.join(cv2.data.haarcascades, 'haarcascade_frontalface_default.xml'))
+#     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+#     faces = facecasc.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
+#     for (x, y, w, h) in faces:
+#         cv2.rectangle(frame, (x, y-50), (x+w, y+h+10), (255, 0, 0), 2)
+#         roi_gray = gray[y:y + h, x:x + w]
+#         cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
+#         prediction = model.predict(cropped_img)
+#         maxindex = int(np.argmax(prediction))
+#         cv2.putText(frame, emotion_dict[maxindex], (frame.shape[0]//8, frame.shape[1]//8), cv2.FONT_HERSHEY_SIMPLEX, 1,
+#                     (0, 255, 0), 2, cv2.LINE_AA)
+#     # cv2.imshow('Recognition', frame)
+#     cv2.waitKey()
+#     return emotion_dict[maxindex]
 
 
 # get_emotion("4_happy.jpg")
